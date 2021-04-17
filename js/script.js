18e4a38b9e71802ba440ebf6fae19be70d8414ed
Kaniz Fatema <77629494+Fatema-Shraboni@users.noreply.github.com>
2021-04-17 12:20:56 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function(){
+   
+  // slick-slider
+  $('.autoplay').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+ });
+
+		
+      
+});
+
